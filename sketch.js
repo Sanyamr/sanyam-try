3921bcc21fc0c64e09ade4233c51a0873cdec335
Sanyamr <99127349+Sanyamr@users.noreply.github.com>
2022-02-26 18:01:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var mam = [1,2,67,54];
+var sum = mam[0] +mam[1]+mam[2]+mam[3];
+var avg = sum/mam.length;
+console.log(avg);
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
